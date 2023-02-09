ded109231fa5d99202fae9275b3fda0456a9597f
Jay Schramm <77761126+jaysch19@users.noreply.github.com>
2023-02-09 13:19:57 -0500

    Working on alignment and orientation

--- src/arduino_code/basic_ultrasonic.ino
+++ src/arduino_code/basic_ultrasonic.ino
@@ -4,38 +4,35 @@ Goal is for there to be six ultrasonic sensors around the robot (2 on the left s
 and create distance calculations of knowing where the robot is relative to the gameboard using a coordinate system
 */
 
-//Front Left trig and echo pins and distance variables
+//Front Left trig and echo pin 
 #define Front_Left_trigPin 4    // Trigger
 #define Front_Left_echoPin 5    // Echo
-int duration_Front_Left,inches_Front_Left;
 
-//Back Left trig and echo pins and distance variables
+//Back Left trig and echo pin
 #define Back_Left_trigPin 3
 #define Back_Left_echoPin 2
-int duration_Back_Left,inches_Back_Left;
 
-//Top Left trig and echo pins and distance variables
+//Top Left trig and echo pin
 #define Top_Left_trigPin 6
 #define Top_Left_echoPin 7
-int duration_Top_Left,inches_Top_Left;
 
-//Front Right trig and echo pins and distance variables
+
+//Front Right trig and echo pin
 #define Front_Right_trigPin 9
 #define Front_Right_echoPin 8
-int duration_Front_Right,inches_Front_Right;
 
-//Back Right trig and echo pins and distance variables
+
+//Back Right trig and echo pin
 #define Back_Right_trigPin 11
 #define Back_Right_echoPin 10
-int duration_Back_Right,inches_Back_Right;
-//Bottom Right trig and echo pins and distance variables
 
+//Bottom Right trig and echo pin
 #define Bottom_Right_trigPin 13
 #define Bottom_Right_echoPin 12
-int duration_Bottom_Right,inches_Bottom_Right;
 
-void setup() {
-  //Serial Port begin
+
+void setup() 
+{
   Serial.begin(9600);
   //Define inputs and outputs (Trig and echo pins) of each of the ultrasonic sensors
   //Front Left
@@ -65,19 +62,75 @@ void setup() {
 void loop() 
 {
   //for each ultrasonic sensor calculate the distance measurement in inches
-  findFrontLeftDistance();
-  findBackLeftDistance();
-  findTopLeftDistance();
-  findFrontRightDistance();
-  findBackRightDistance();
-  findBottomRightDistance();
-
+  int FL = findFrontLeftDistance();
+  int BL = findBackLeftDistance();
+  int TL = findTopLeftDistance();
+  int FR = findFrontRightDistance();
+  int BaR = findBackRightDistance();
+  int BoR = findBottomRightDistance();
+  Serial.println(FL);
+  Serial.println(BL);
+  Serial.println(TL);
+  Serial.println(FR);
+  Serial.println(BaR);
+  Serial.println(BoR);
+  //now locate the robot based on the ultrasonic measurements to relative gameboard locations
+  //Check the allignment of the robot
+  alligned(FL,BL,FR,BaR);
+  //check the orientation of the robot, if it vertical or horizontal
+  orientation(FL,BL,FR,BaR,TL,BoR);
+  // DUCK POND SIDE LOCATIONS
+  if ((FL >= 26 && FL <= 28) && (BL >= 24 && BL <= 28) && (TL <= 8) && (BoR >= 23 && BoR <= 27))
+  {
+    Serial.println("Left duck pond");
+  }
+  if ((FR >= 35 && FR <= 39) && (BaR >= 35 && BaR <= 39) && (TL <= 20) && (BoR >= 12 && BoR <= 16))
+  {
+    Serial.println("Bottom duck pond");
+  }
   
 
 }
-//for each of the sensor measurements followed reference listed in comment header
-void findFrontLeftDistance()
+
+void orientation(int front_left,int back_left,int front_right,int back_right, int top_left, int bottom_right);
+{//Determines the position of the robot and if it vertical or horizontal
+    
+  if((front_left||back_left||front_right||back_right) > (top_left||bottom_right))
+  {
+    Serial.println("Orientation is vertical");
+  }
+  else
+  {
+   Serial.println("Orientation is Horizontal"); 
+  }
+}
+void alligned(int front_left,int back_left,int front_right,int back_right);
 {
+  if(front_left > back_left)
+  {
+    Serial.println("Robot is Diaginal Left: rotate front left CCW");
+  }
+  else if (back_left > front_left)
+  {
+   Serial.println("Robot is Diaginal Left: rotate back left CW"); 
+  }
+  else if(front_right > back_right)
+  {
+    Serial.println("Robot is Diaginal Right: rotate front right CW");
+  }
+  else if(back_right > front_right)
+  {
+    Serial.println("Robot is Diaginal Right: rotate back right CCW");
+  }
+  else
+  {
+    Serial.println("Robot is alligned");
+  }
+}
+//for each of the sensor measurements followed reference listed in comment header
+int findFrontLeftDistance()
+{
+  int duration_Front_Left,inches_Front_Left;
   digitalWrite(Front_Left_trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(Front_Left_trigPin, HIGH);
@@ -87,14 +140,16 @@ void findFrontLeftDistance()
   duration_Front_Left = pulseIn(Front_Left_echoPin, HIGH);
   // Convert the time into a distance
   inches_Front_Left = (duration_Front_Left/2) / 74;   // Divide by 74 or multiply by 0.0135
-  Serial.print(inches_Front_Left);
-  Serial.println(" Inches at Front Left Sensor");
+//  Serial.print(inches_Front_Left);
+//  Serial.println(" Inches at Front Left Sensor");
   delay(1000);
+  return inches_Front_Left; 
 
 }
-void findBackLeftDistance()
+int findBackLeftDistance()
 {
 //below is back left calculations
+  int duration_Back_Left,inches_Back_Left;
   digitalWrite(Back_Left_trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(Back_Left_trigPin, HIGH);
@@ -103,13 +158,15 @@ void findBackLeftDistance()
   pinMode(Back_Left_echoPin, INPUT);
   duration_Back_Left = pulseIn(Back_Left_echoPin, HIGH);
   inches_Back_Left = (duration_Back_Left/2) / 74;   // Divide by 74 or multiply by 0.0135
-  Serial.print(inches_Back_Left);
-  Serial.println(" Inches at Back Left Sensor");
+//  Serial.print(inches_Back_Left);
+//  Serial.println(" Inches at Back Left Sensor");
   delay(1000);
+  return inches_Back_Left; 
 }
-void findTopLeftDistance()
+int findTopLeftDistance()
 {
  //below is Top Left calculations
+  int duration_Top_Left,inches_Top_Left;
   digitalWrite(Top_Left_trigPin,LOW);
   delayMicroseconds(5);
   digitalWrite(Top_Left_trigPin, HIGH);
@@ -118,12 +175,14 @@ void findTopLeftDistance()
   pinMode(Top_Left_echoPin, INPUT);
   duration_Top_Left = pulseIn(Top_Left_echoPin, HIGH);
   inches_Top_Left = (duration_Top_Left/2) / 74;  
-  Serial.print(inches_Top_Left);
-  Serial.println(" Inches at Top Left Sensor");
+//  Serial.print(inches_Top_Left);
+//  Serial.println(" Inches at Top Left Sensor");
   delay(1000);
+  return inches_Top_Left; 
 }
-void findFrontRightDistance()
+int findFrontRightDistance()
 {
+  int duration_Front_Right,inches_Front_Right;
   digitalWrite(Front_Right_trigPin,LOW);
   delayMicroseconds(5);
   digitalWrite(Front_Right_trigPin, HIGH);
@@ -132,12 +191,14 @@ void findFrontRightDistance()
   pinMode(Front_Right_echoPin, INPUT);
   duration_Front_Right = pulseIn(Front_Right_echoPin, HIGH);
   inches_Front_Right = (duration_Front_Right/2) / 74;  
-  Serial.print(inches_Front_Right);
-  Serial.println(" Inches at Front Right Sensor");
+//  Serial.print(inches_Front_Right);
+//  Serial.println(" Inches at Front Right Sensor");
   delay(1000);
+  return inches_Front_Right; 
 }
-void findBackRightDistance()
+int findBackRightDistance()
 {
+  int duration_Back_Right,inches_Back_Right;
   digitalWrite(Back_Right_trigPin,LOW);
   delayMicroseconds(5);
   digitalWrite(Back_Right_trigPin, HIGH);
@@ -146,13 +207,15 @@ void findBackRightDistance()
   pinMode(Back_Right_echoPin, INPUT);
   duration_Back_Right = pulseIn(Back_Right_echoPin, HIGH);
   inches_Back_Right = (duration_Back_Right/2) / 74;  
-  Serial.print(inches_Back_Right);
-  Serial.println(" Inches at Back Right Sensor");
-  delay(1000); 
+//  Serial.print(inches_Back_Right);
+//  Serial.println(" Inches at Back Right Sensor");
+  delay(1000);
+  return inches_Back_Right; 
 }
 
-void findBottomRightDistance()
+int findBottomRightDistance()
 {
+  int duration_Bottom_Right,inches_Bottom_Right;
   digitalWrite(Bottom_Right_trigPin,LOW);
   delayMicroseconds(5);
   digitalWrite(Bottom_Right_trigPin, HIGH);
@@ -161,8 +224,9 @@ void findBottomRightDistance()
   pinMode(Bottom_Right_echoPin, INPUT);
   duration_Bottom_Right = pulseIn(Bottom_Right_echoPin, HIGH);
   inches_Bottom_Right = (duration_Bottom_Right/2) / 74;  
-  Serial.print(inches_Bottom_Right);
-  Serial.println(" Inches at Bottom Right Sensor");
-  delay(1000); 
+//  Serial.print(inches_Bottom_Right);
+//  Serial.println(" Inches at Bottom Right Sensor");
+  delay(1000);
+  return inches_Bottom_Right; 
 
 }
