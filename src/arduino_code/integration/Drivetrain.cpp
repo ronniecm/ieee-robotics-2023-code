57832d204e9f60e32cf65e80acce7fb1909bf4e0
Ronnie Mohapatra <ronniecm@gmail.com>
2023-02-10 13:47:37 -0500

    Just for you jhonny v <3333

--- src/arduino_code/integration/Drivetrain.cpp
+++ src/arduino_code/integration/Drivetrain.cpp
@@ -14,11 +14,13 @@
 
 Drivetrain::Drivetrain()
 {
+    //First initialize all 4 encoder objects
     enc[0] = new Encoder(23,22);
     enc[1] = new Encoder(34, 33);
     enc[2] = new Encoder(1, 2);
     enc[3] = new Encoder(32, 31);
 
+    //Next initialize all 4 PID speed controllers with proper PID and setpoint values
     for (int i = 0; i < 4; i++)
     {
         speedController[i] = new PID(&in[i], &out[i], &setpoint[i], kP[i], kI, kD[i], DIRECT);
@@ -26,43 +28,51 @@ Drivetrain::Drivetrain()
         speedController[i]->SetSampleTime(10);
     }
 
+    //initialize count to 0
     count = 0;
 }
 
 Drivetrain::~Drivetrain()
 {
+    // Destructor deletes any pointers that were used
     delete[] enc;
     delete[] speedController;
 }
 
 void Drivetrain::mecanumDrive(float x, float y, float z)
 {
+    // calculate power for each wheel with respective formula
     double frontLeft = y + x + z;
     double frontRight = y - x - z;
     double backLeft = y - x + z;
     double backRight = y + x - z;
 
+    // map -1.0 to 1.0 range to -100 to 100 RPM
     double frontLeftRPM = frontLeft * 200.0 / 2.0;
     double frontRightRPM = frontRight * 200.0 / 2.0;
     double backLeftRPM = backLeft * 200.0 / 2.0;
     double backRightRPM = backRight * 200.0 / 2.0;
-    
+
+    /*
     frontLeft = frontLeft * 255;
     frontRight = frontRight * 255;
     backLeft = backLeft * 255;
     backRight = backRight * 255;
+    */
 
+    // set the setpoint of each PID speed controller
     setpoint[0] = abs(frontLeftRPM);
     setpoint[1] = abs(frontRightRPM);
     setpoint[2] = abs(backLeftRPM);
     setpoint[3] = abs(backRightRPM);
 
+    // set the input of each PID speed controller and compute the output
     for (int i = 0; i < 4; i++)
     {
         in[i] = abs(finalRpm[i]);
         speedController[i]->Compute();
     }
-    
+
     // Apply the calculated values to the motor control pins
     if (frontLeft >= 0)
     {
@@ -108,9 +118,15 @@ void Drivetrain::mecanumDrive(float x, float y, float z)
 
 void Drivetrain::calcRPM()
 {
+    //this functions gets called every millisecond
+    //everytime the functions gets called increment the call count variable 'count'
     count++;
     for (int i = 0; i < 4; i++)
     {
+        //for each motor, find its current encoder position and then how much it has changed from its last reading
+        //then divide that delta value by TICK_PER_REV to get the number of revolutions performed in that millisecond
+        //finally to retrieve the RPM value multiply that value by 60,000 and set the prevPos[i] to the currPos saved earlier
+        //every 10 calls to this functions, the samples are averaged and saved into finalRpm array
         int currPos = enc[i]->read();
         int deltaPos = currPos - prevPos[i];
         double revs = deltaPos / 2678.0;
--- src/arduino_code/integration/Drivetrain.h
+++ src/arduino_code/integration/Drivetrain.h
@@ -4,29 +4,36 @@
 #include <Arduino.h>
 #include <vector>
 
+/*
+ * Author: Ronnie Mohapatra 
+ * Description: Outlines header file for Drivetrain object
+ */
 class Drivetrain {
 public:
-    Drivetrain();
-    ~Drivetrain();
-    Encoder* getEnc(int i);
-    PID* getController(int i);
-    double getRPM(int i);
-    void mecanumDrive(float x, float y, float z);
-    void calcRPM();
+    Drivetrain(); //constructor
+    ~Drivetrain(); //destructor
+    Encoder* getEnc(int i); //get method for an encoder object
+    PID* getController(int i); //get method for a PID speed controller
+    double getRPM(int i); //get method for retrieving RPM of a wheel
+    void mecanumDrive(float x, float y, float z); //mecanum drive function
+    void calcRPM(); //calculates RPM of all motors
 private:
-    Encoder* enc[4];
-    int prevPos[4];
-    int count;
+    Encoder* enc[4]; //encoder array
+    int prevPos[4]; //used in RPM calculations
+    int count; //used in RPM calculations
 
-    double rpmSum[4];
-    double finalRpm[4];
+    double rpmSum[4]; //used in RPM calculations
+    double finalRpm[4]; //official RPM array for all motors
 
+    //PID values for all 4 speed controllers
     double kP[4] = {30,30,30,30};
     double kI = 0.0;
     double kD[4] = {0,0,0,0};
+
+    //Input, output, and setpoint arrays for all PID speed controllers
     double in[4];
     double out[4];
     double setpoint[4];
 
-    PID* speedController[4];
+    PID* speedController[4]; //array of PID speed controllers for all motors
 };
