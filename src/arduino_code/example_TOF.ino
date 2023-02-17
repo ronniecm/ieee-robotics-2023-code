180e8c2fc28bcc0d73e1f1c52ab9a6967c584995
aritaortuno <77861257+aritaortuno@users.noreply.github.com>
2023-02-17 17:21:28 -0500

    Updated code for 2 ToF sensors (VL6180X)

--- src/arduino_code/example_TOF.ino
+++ src/arduino_code/example_TOF.ino
@@ -1,70 +1,256 @@
+/*
+ *  Arduino code 
+* Lesson 76-2: Using two or more VL6180 Laser  Distance Sensor with Arduino 
+ * Adafruit code modified for this tutorial 
+Using two or more VL6180X 20cm Time-of-Flight proximity sensor with Arduino
+
+View code for using single VL6180X sensors: https://robojax.com/course1/lecture76
+
+ * Original code and library by https://github.com/adafruit/Adafruit_VL6180X
+ * 
+ * Written/updated by Ahmad Shamshiri for Robojax Robojax.com
+ * on Mar 12, 2021  in Ajax, Ontario, Canada
+ Watch the video instruction for this sketch: https://youtu.be/_H9D0czQpSI
+ 
+
+
+Please watch video instruciton of this code : https://youtu.be/_H9D0czQpSI
+ 
+
+  This video is part of Arduino Step by Step Course which starts here: https://youtu.be/-6qSrDUA5a8
+
+ 
+
+If you found this tutorial helpful, please support me so I can continue creating 
+content like this. You can support me on Patreon http://robojax.com/L/?id=63
+
+or make donation using PayPal http://robojax.com/L/?id=64
+  
+ * This code is "AS IS" without warranty or liability. Free to be used as long as you keep this note intact.* 
+ * This code has been download from Robojax.com
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+
+*/
+
 #include <Adafruit_VL6180X.h>
 
-//VL6180X Time of Flight Sensor
-//Download Library: https://learn.adafruit.com/adafruit-vl6180x-time-of-flight-micro-lidar-distance-sensor-breakout/wiring-and-test?view=all
-#include <Wire.h>
-#include "Adafruit_VL6180X.h"
+// address we will assign if dual sensor is present
+#define LOX1_ADDRESS 0x30
+#define LOX2_ADDRESS 0x31
 
-Adafruit_VL6180X vl = Adafruit_VL6180X();
 
-void setup() {
-  Serial.begin(115200);
 
-  // wait for serial port to open on native usb devices
-  while (!Serial) {
-    delay(1);
-  }
-  
-  Serial.println("Adafruit VL6180x test!");
-  if (! vl.begin()) {
-    Serial.println("Failed to find sensor");
+// set the pins to shutdown
+#define SHT_LOX1 7
+#define SHT_LOX2 6
+
+
+// Optional define GPIO pins to check to see if complete
+#define GPIO_LOX1 4
+#define GPIO_LOX2 3
+
+
+#define TIMING_PIN 13
+
+// objects for the VL6180X
+Adafruit_VL6180X lox1 = Adafruit_VL6180X();
+Adafruit_VL6180X lox2 = Adafruit_VL6180X();
+
+
+// Setup mode for doing reads
+typedef enum {RUN_MODE_DEFAULT, RUN_MODE_TIMED, RUN_MODE_ASYNC, RUN_MODE_GPIO, RUN_MODE_CONT} runmode_t;
+
+runmode_t run_mode = RUN_MODE_DEFAULT;
+uint8_t show_command_list = 1;
+
+//==========================================================================
+// Define some globals used in the continuous range mode
+// Note: going to start table drive this part, may back up and do the rest later
+Adafruit_VL6180X *sensors[] = {&lox1, &lox2};
+const uint8_t COUNT_SENSORS = sizeof(sensors) / sizeof(sensors[0]);
+const int sensor_gpios[COUNT_SENSORS] = {GPIO_LOX1, GPIO_LOX2}; // if any are < 0 will poll instead
+uint8_t  tempRange;
+float         sensor_ranges[COUNT_SENSORS];
+float         sensor_status[COUNT_SENSORS];
+// Could do with uint8_t for 8 sensors, but just in case...
+const uint16_t  ALL_SENSORS_PENDING = ((1 << COUNT_SENSORS) - 1);
+uint16_t        sensors_pending = ALL_SENSORS_PENDING;
+uint32_t        sensor_last_cycle_time;
+
+
+/*
+    Reset all sensors by setting all of their XSHUT pins low for delay(10), then set all XSHUT high to bring out of reset
+    Keep sensor #1 awake by keeping XSHUT pin high
+    Put all other sensors into shutdown by pulling XSHUT pins low
+    Initialize sensor #1 with lox.begin(new_i2c_address) Pick any number but 0x29 and it must be under 0x7F. Going with 0x30 to 0x3F is probably OK.
+    Keep sensor #1 awake, and now bring sensor #2 out of reset by setting its XSHUT pin high.
+    Initialize sensor #2 with lox.begin(new_i2c_address) Pick any number but 0x29 and whatever you set the first sensor to
+*/
+void setID() {
+  // all reset
+  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX2, LOW);
+
+  delay(10);
+
+  // all unreset
+  digitalWrite(SHT_LOX1, HIGH);
+  digitalWrite(SHT_LOX2, HIGH);
+
+  delay(10);
+
+  // activating LOX1 and reseting LOX2
+  digitalWrite(SHT_LOX1, HIGH);
+  digitalWrite(SHT_LOX2, LOW);
+
+
+  // initing LOX1
+  if (!lox1.begin()) {
+    Serial.println(F("Failed to boot first VL6180X"));
     while (1);
   }
-  Serial.println("Sensor found!");
+  lox1.setAddress(LOX1_ADDRESS);
+  delay(10);
+
+  // activating LOX2
+  digitalWrite(SHT_LOX2, HIGH);
+  delay(10);
+
+  //initing LOX2
+  if (!lox2.begin()) {
+    Serial.println(F("Failed to boot second VL6180X"));
+    while (1);
+  }
+  lox2.setAddress(LOX2_ADDRESS);
+  delay(10);
+
+ 
 }
 
-void loop() {
+void readSensor(Adafruit_VL6180X &vl) {
+
   float lux = vl.readLux(VL6180X_ALS_GAIN_5);
 
-  //Serial.print("Lux: "); Serial.println(lux);
-  
   float range = vl.readRange();
+
   uint8_t status = vl.readRangeStatus();
 
   if (status == VL6180X_ERROR_NONE) {
-    float to_cm = range/10;
-    Serial.print("Range: "); Serial.println(to_cm);
-
+      tempRange = range;//save it for the moment
   }
 
   // Some error occurred, print it out!
-  
+
   if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
-    Serial.println("System error");
+    Serial.print("(System error)");
   }
   else if (status == VL6180X_ERROR_ECEFAIL) {
-    Serial.println("ECE failure");
+    Serial.print("(ECE failure)");
   }
   else if (status == VL6180X_ERROR_NOCONVERGE) {
-    Serial.println("No convergence");
+    Serial.print("(No convergence)");
   }
   else if (status == VL6180X_ERROR_RANGEIGNORE) {
-    Serial.println("Ignoring range");
+    Serial.print("(Ignoring range)");
   }
   else if (status == VL6180X_ERROR_SNR) {
-    Serial.println("Signal/Noise error");
+    Serial.print("Signal/Noise error");
   }
   else if (status == VL6180X_ERROR_RAWUFLOW) {
-    Serial.println("Raw reading underflow");
+    Serial.print("Raw reading underflow");
   }
   else if (status == VL6180X_ERROR_RAWOFLOW) {
-    Serial.println("Raw reading overflow");
+    Serial.print("Raw reading overflow");
   }
   else if (status == VL6180X_ERROR_RANGEUFLOW) {
-    Serial.println("Range reading underflow");
+    Serial.print("Range reading underflow");
   }
   else if (status == VL6180X_ERROR_RANGEOFLOW) {
-    Serial.println("Range reading overflow");
+    Serial.print("Range reading overflow");
   }
-  delay(50);
+}
+
+void read_sensors() {
+  readSensor(lox1);
+  sensor_ranges[0]=tempRange;//save it now
+
+  readSensor(lox2);
+  sensor_ranges[1]=tempRange; //save it now 
+
+  Serial.println();
+}
+
+
+
+
+
+//===============================================================
+// Setup
+//===============================================================
+void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+
+  pinMode(SHT_LOX1, OUTPUT);
+  pinMode(SHT_LOX2, OUTPUT);
+
+
+  // Enable timing pin so easy to see when pass starts and ends
+  pinMode(TIMING_PIN, OUTPUT);
+
+#ifdef GPIO_LOX1
+  // If we defined GPIO pins, enable them as PULL UP
+  pinMode(GPIO_LOX1, INPUT_PULLUP);
+  pinMode(GPIO_LOX2, INPUT_PULLUP);
+
+#endif
+
+  Serial.println("Shutdown pins inited...");
+
+  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX2, LOW);
+
+  digitalWrite(TIMING_PIN, LOW);
+  Serial.println("All in reset mode...(pins are low)");
+
+
+  Serial.println("Starting...");
+  setID();
+
+}
+
+//===============================================================
+// Loop
+//===============================================================
+void loop() {
+ read_sensors();
+      for(int i=0; i<COUNT_SENSORS; i++)
+      {
+        Serial.print("Sensor ");
+        Serial.print(i);
+        Serial.print(" :");        
+        Serial.print(sensor_ranges[i] /10);
+        Serial.print("cm");
+        Serial.println();
+      } 
+//      if(sensor_ranges[1] >=76)
+//      {
+//        //do something here
+//      }
+  delay(100);
 }
