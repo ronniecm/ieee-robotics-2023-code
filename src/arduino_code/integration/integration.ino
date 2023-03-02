48ed1491adfbc027d722fd16a8542e96ed796440
Juan <suqjuan@vt.edu>
2023-03-02 00:33:53 -0500

    Arm integration

--- src/arduino_code/integration/integration.ino
+++ src/arduino_code/integration/integration.ino
@@ -1,12 +1,9 @@
 #include "Drivetrain.h"
 #include "ros.h"
 #include "geometry_msgs/Twist.h"
-
-//Setting up the ros node for wheel commands
-ros::NodeHandle nh;
-//Thie type of message is geometry_msgs::Twist
-geometry_msgs::Twist msg;
-
+#include "amc.h"
+#include "TeensyThreads.h"
+#include "std_msgs/Int16.h"
 
 #define FL_in1 2
 #define FL_in2 3
@@ -20,13 +17,98 @@ geometry_msgs::Twist msg;
 #define BR_in1 28
 #define BR_in2 29
 
-Drivetrain *drivetrain;
+//Setting up the ros node for wheel commands
+ros::NodeHandle nh;
+//Thie type of message is geometry_msgs::Twist
+geometry_msgs::Twist msg;
 
+
+Drivetrain *drivetrain;
+Amc* arm;
+
+std_msgs::Int16 gripperRotateMsg;
+std_msgs::Int16 gripperClampMsg;
+std_msgs::Int16 doorMsg;
+std_msgs::Int16 armMsg;
+std_msgs::Int16 wristMsg;
+std_msgs::Int16 paddleMsg;
+std_msgs::Int16 liftingMsg;
+std_msgs::Int16 carouselMsg;
+std_msgs::Int16 cmd_msg;
+
+std_msgs::Int16 gripperRotateCmd;
+std_msgs::Int16 gripperClampCmd;
+std_msgs::Int16 doorCmd;
+std_msgs::Int16 armCmd;
+std_msgs::Int16 wristCmd;
+std_msgs::Int16 paddleCmd;
+std_msgs::Int16 liftingCmd;
+std_msgs::Int16 carouselCmd;
 
 //These will be the inputs to the mecanumDrive function
 float cmd_x;
 float cmd_y;
 float cmd_z;
+
+ros::Publisher GripperRotate("/bot/gripperRotate_callback", &gripperRotateMsg);
+ros::Publisher GripperClamp("/bot/gripperClamp_callback", &gripperClampMsg);
+ros::Publisher Door("/bot/door_callback", &doorMsg);
+ros::Publisher Arm("/bot/arm_callback", &armMsg);
+ros::Publisher Wrist("/bot/wrist_callback", &wristMsg);
+ros::Publisher Paddle("/bot/paddle_callback", &paddleMsg);
+ros::Publisher Lifting("/bot/lifting_callback", &liftingMsg);
+ros::Publisher Carousel("/bot/carousel_callback", &carouselMsg);
+
+
+void gripperRotateCB(const std_msgs::Int16& cmd_msg)
+{
+  gripperRotateCmd.data = cmd_msg.data;
+}
+
+void gripperClampCB(const std_msgs::Int16& cmd_msg)
+{
+  gripperClampCmd.data = cmd_msg.data;
+}
+
+void doorCB(const std_msgs::Int16& cmd_msg)
+{
+  doorCmd.data = cmd_msg.data;
+}
+
+void armCB(const std_msgs::Int16& cmd_msg)
+{
+  armCmd.data = cmd_msg.data;
+}
+
+void wristCB(const std_msgs::Int16& cmd_msg)
+{
+  wristCmd.data = cmd_msg.data;
+}
+
+void paddleCB(const std_msgs::Int16& cmd_msg)
+{
+  paddleCmd.data = cmd_msg.data;
+}
+
+void liftingCB(const std_msgs::Int16& cmd_msg)
+{
+  liftingCmd.data = cmd_msg.data;
+}
+
+void carouselCB(const std_msgs::Int16& cmd_msg)
+{
+  carouselCmd.data = cmd_msg.data;
+}
+
+ros::Subscriber <std_msgs::Int16> gripperRotate("/bot/gripperRotate_cmd", &gripperRotateCB);
+ros::Subscriber <std_msgs::Int16> gripperClam("/bot/gripperClamp_cmd", &gripperClampCB);
+ros::Subscriber <std_msgs::Int16> door("/bot/door_cmd", &doorCB);
+ros::Subscriber <std_msgs::Int16> elbow("/bot/arm_cmd", &armCB);
+ros::Subscriber <std_msgs::Int16> wrist("/bot/wrist_cmd", &wristCB);
+ros::Subscriber <std_msgs::Int16> paddle("/bot/paddle_cmd", &paddleCB);
+ros::Subscriber <std_msgs::Int16> lifting("/bot/lifting_cmd", &liftingCB);
+ros::Subscriber <std_msgs::Int16> carousel("/bot/carousel_cmd", &carouselCB);
+
 
 //This will be the callback function for wheel commands from jetson
 void mecanumDriveCallBack(const geometry_msgs::Twist& cmd_msg)
@@ -66,12 +148,39 @@ double demand;
 void setup()
 {
     drivetrain = new Drivetrain();
+    arm = new Amc;
 
     initDrivetrain();
     //Ros setup node & subscribe to topic
     nh.initNode();
     nh.subscribe(sub);
+
+    nh.initNode();
     
+    nh.subscribe(gripperRotate);
+    
+    nh.subscribe(gripperClamp);
+    
+    nh.subscribe(door);
+    
+    nh.subscribe(elbow);
+    
+    nh.subscribe(wrist);
+    
+    nh.subscribe(spaddle);
+    
+    nh.subscribe(lifting);
+    
+    nh.subscribe(carousel);
+
+    gripperRotateCmd.data = 90;
+    gripperClampCmd.data = 0;
+    wristCmd.data = 180;
+    armCmd.data = 180;
+    paddleCmd.data = 180;
+    doorCmd.data = 100;
+    carouselCmd.data = 0;
+      
     xController.SetMode(AUTOMATIC);
     xController.SetOutputLimits(-1.0, 1.0);
     xController.SetSampleTime(10);
@@ -83,6 +192,9 @@ void setup()
     yController.SetMode(AUTOMATIC);
     yController.SetOutputLimits(-1.0, 1.0);
     yController.SetSampleTime(10);
+    pinMode(33, OUTPUT);
+    pinMode(38, INPUT);
+    pinMode(39, INPUT);
 }
 
 void loop()
@@ -98,9 +210,32 @@ void loop()
      drivetrain->mecanumDrive(cmd_y, cmd_x,cmd_z);
      for(int i = 0; i < 4; i++) {
         Serial.print(drivetrain->getRPM(i));
-        Serial.print(" ");
+        //Serial.print(" ");
      }
-     Serial.println();
+     //Serial.println();
+     
+  arm->gripperRotateCmd(gripperRotateCmd.data);
+  GripperRotate.publish(&gripperRotateCmd);
+  
+  arm->gripperClampCmd(gripperClampCmd.data);
+  GripperClamp.publish(&gripperClampCmd);
+
+  arm->doorCmd(doorCmd.data);
+  Door.publish(&doorCmd);
+  
+  arm->armCmd(armCmd.data);
+  Arm.publish(&armCmd);
+  
+  arm->wristCmd(wristCmd.data);
+  Wrist.publish(&wristCmd);
+
+  arm->paddleCmd(paddleCmd.data);
+  Paddle.publish(&paddleCmd);
+  
+  arm->liftingCmd(liftingCmd.data);
+  
+  arm->carouselCmd(carouselCmd.data);
+  Carousel.publish(&carouselCmd);
    }
 
    nh.spinOnce();
