691b4b1d229e326f97ba9d4d3189c17cc6996992
Juan <suqjuan@vt.edu>
2023-03-02 02:37:49 -0500

    integration

---
+++ src/arduino_code/integration/amc.h
@@ -0,0 +1,83 @@
+#ifndef AMC_H
+#define AMC_H
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Encoder.h>
+//#include <nRF24L01.h>
+//#include <RF24.h>
+#include "Adafruit_TCS34725.h"
+#include "Arduino.h"
+
+
+
+
+
+class Amc {
+  public:
+    Amc();
+    ~Amc();
+    void activate_paddle();
+    void stepperContinue();
+    void dispense_stack_helper();
+    void dispense_helper(int i, int pedestal);
+    void dispense();
+    void drop_in_action();
+    void update_slots(int dir);
+    void storeUprightPedestal(const int msg);
+    void liftArm();
+    void lowerArm();
+
+
+    //Functions for all servo controll
+    void gripperRotateCmd(int angle);
+    void gripperClampCmd(int angle);
+    void doorCmd(int angle);
+    void armCmd(int angle);
+    void wristCmd(int angle);
+    void paddleCmd(int angle);
+    void liftingCmd(int angle);
+    void carouselCmd(int angle);
+  
+
+
+
+    
+    int deg1;
+    int deg2;
+    int deg3;
+    int deg4;
+    int deg5;
+    int deg6;
+    
+  private:
+    Adafruit_PWMServoDriver* servos;
+    Adafruit_PWMServoDriver* steppers;
+    Adafruit_TCS34725* tcs;
+    char input;
+
+    int lift;
+    int stepsLeft;
+    int carouselSpeed;
+    int speedControl;
+    int slots[5];
+    int tube[3];
+    int built[3];
+    int temp;
+    bool drop_in;
+    bool dispense_stack;
+    bool dispensed;
+    bool dispense_stack_start;
+    bool dispense_stack_finish;
+    int drive_state;
+    uint16_t r;
+    uint16_t g;
+    uint16_t b;
+    uint16_t c;
+    uint16_t colorTemp;
+    uint16_t lux;
+};
+
+#endif
