632d51ef4bff900e07eb61a97216209dce1d6d57
Juan <suqjuan@vt.edu>
2023-03-06 18:36:02 -0500

    Camera Stuff

--- src/jetson_code/Cameras.py
+++ src/jetson_code/Cameras.py
@@ -38,7 +38,7 @@ import numpy as np
 from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput, logUsage
 import jetson_utils_python
-from math import sqrt
+from math import sqrt, degrees, pi, atan2
 import threading
 
 class RealSense:
@@ -84,6 +84,15 @@ class RealSense:
 
 		self.detections = []
 
+		#This for Yaw calculation thread
+		self.angles = [0.0, 0.0, 0.0]
+		self.first = True
+		self.alpha = 0.98
+		self.totalgyroangleY = 0
+
+		angles= threading.Thread(target=self.angleCalculation)
+
+
 		# create video sources and outputs
 		# input = videoSource(args.input_URI, argv=sys.argv)
 		self.output = videoOutput("display://0")
@@ -96,8 +105,9 @@ class RealSense:
                  input_blob="input_0", output_cvg="scores", output_bbox="boxes",
                  threshold=self.args.threshold)
 		
-		thread = threading.Thread(target=self.run)
-		thread.start()
+		#thread = threading.Thread(target=self.objDectect)
+		#thread.start()
+		angles.start()
 		
 
 	def filter_detections(self, detections):
@@ -137,7 +147,7 @@ class RealSense:
 		else:
 			return self.detectionData
 
-	def run(self):
+	def objDectect(self):
 		
 		while True:
 			# Real Sense frame data: Wait for a coherent pair of frames: depth and color
@@ -206,6 +216,94 @@ class RealSense:
 			# exit on input/output EOS
 			if not self.output.IsStreaming():
 				break
+
+
+	"""
+	This script is based on the code from the following sources:
+	https://github.com/IntelRealSense/librealsense/tree/master/examples/motion
+	https://github.com/IntelRealSense/librealsense/issues/4391
+
+
+
+	Code explanation from ChatGPT:
+
+	A complementary filter is a type of filter that combines information from two or more sources to produce a 
+	more accurate estimate of a signal or a system's behavior. The term "complementary" refers to the fact that 
+	the filter combines information from two or more sources that are complementary to each other, meaning that 
+	they provide information about different aspects of the signal or system.
+
+	A complementary filter is often used to fuse information from multiple sensors, such as an accelerometer 
+	and a gyroscope, to estimate the orientation of a device, for example. The accelerometer provides information 
+	about the linear acceleration of the device, while the gyroscope provides information about the angular 
+	velocity. By combining these two sources of information, a complementary filter can produce a more accurate 
+	estimate of the orientation of the device.
+
+	The basic idea behind a complementary filter is to use a low-pass filter to smooth out the noisy accelerometer 
+	measurements and a high-pass filter to capture the fast changes in orientation provided by the gyroscope. 
+	The two filters are then combined using a simple weighted sum to produce the final estimate of the orientation.
+
+	Complementary filters are widely used in many applications, including inertial navigation systems, 
+	robotics, and aerospace engineering, where accurate estimation of signals or system behavior is critical. 
+	They are simple to implement and can be easily adapted to different applications by adjusting 
+	the filter parameters.
+
+	"""
+	def angleCalculation(self):
+		f = self.pipeline.wait_for_frames()
+
+        #gather IMU data
+        accel = f[0].as_motion_frame().get_motion_data()
+        gyro = f[1].as_motion_frame().get_motion_data()
+
+        ts = f.get_timestamp()
+
+        #calculation for the first frame
+        if (first):
+            first = False
+            last_ts_gyro = ts
+
+            # accelerometer calculation
+            accel_angle_z = degrees(atan2(accel.y, accel.z))
+            accel_angle_x = degrees(atan2(accel.x, sqrt(accel.y * accel.y + accel.z * accel.z)))
+            accel_angle_y = degrees(pi)
+            continue
+
+        #calculation for the second frame onwards
+
+        # gyrometer calculations
+        dt_gyro = (ts - last_ts_gyro) / 1000
+        last_ts_gyro = ts
+
+        gyro_angle_x = gyro.x * dt_gyro
+        gyro_angle_y = gyro.y * dt_gyro
+        gyro_angle_z = gyro.z * dt_gyro
+
+        dangleX = gyro_angle_x * 57.2958
+        dangleY = gyro_angle_y * 57.2958
+        dangleZ = gyro_angle_z * 57.2958
+
+        totalgyroangleX = accel_angle_x + dangleX
+        # totalgyroangleY = accel_angle_y + dangleY
+        totalgyroangleY = accel_angle_y + dangleY + totalgyroangleY
+        totalgyroangleZ = accel_angle_z + dangleZ
+
+        #accelerometer calculation
+        accel_angle_z = degrees(atan2(accel.y, accel.z))
+        accel_angle_x = degrees(atan2(accel.x, sqrt(accel.y * accel.y + accel.z * accel.z)))
+        # accel_angle_y = math.degrees(math.pi)
+        accel_angle_y = 0
+
+        #combining gyrometer and accelerometer angles
+        combinedangleX = totalgyroangleX * alpha + accel_angle_x * (1-alpha)
+        combinedangleZ = totalgyroangleZ * alpha + accel_angle_z * (1-alpha)
+        combinedangleY = totalgyroangleY
+
+		self.angleData = [combinedangleX, combinedangleY - 180, combinedangleZ]
+	
+
+	def getCurrYaw(self):
+		return self.angleData[1]
+
 		
 
 def getScore(distance, class_id):
